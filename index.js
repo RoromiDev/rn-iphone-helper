e0d54bd6cab1688e8df5bedb4e0824711d7dea6d
Alexander Pataridze <s.pataridze@gmail.com>
2022-11-27 20:42:14 +0400

    refactor: add legacy mode device identifier

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ const EMPTY_OBJECT = {};
 let topInset = 47;
 let device = null;
 
+function checkDimensions(portraitWidth, portraitHeight) {
+  const window = Dimensions.get('window');
+  const windowRes =
+    (window.height === portraitHeight && window.width === portraitWidth) ||
+    (window.width === portraitHeight && window.height === portraitWidth);
+
+  const screen = Dimensions.get('screen');
+  const screenRes =
+    (screen.height === portraitHeight && screen.width === portraitWidth) ||
+    (screen.width === portraitHeight && screen.height === portraitWidth);
+
+  return windowRes || screenRes;
+}
+
 function loadDeviceId() {
   if (isAndroid) return;
 
@@ -38,6 +52,21 @@ function loadDeviceId() {
     } catch (_) {}
   }
 
+  if (!device) {
+    if (hasDisplayCutout()) {
+      if (checkDimensions(390, 844) || checkDimensions(428, 926)) {
+        topInset = 47;
+      } else if (checkDimensions(360, 780)) {
+        topInset = 50;
+      } else if (checkDimensions(393, 852) || checkDimensions(430, 932)) {
+        topInset = 59;
+      } else {
+        topInset = 44;
+      }
+      device = deviceIdToProps[insetToDeviceId[topInset]];
+    }
+  }
+
   let warningText = '';
   if (!device) {
     if (!deviceId) {
@@ -54,24 +83,25 @@ loadDeviceId();
 function _hasNotchLegacy() {
   return (
     isIphone &&
-    (checkDemension(780) ||
-      checkDemension(812) ||
-      checkDemension(844) ||
-      checkDemension(896) ||
-      checkDemension(926) ||
-      checkDemension(852) ||
-      checkDemension(932))
+    (checkDimension(780) ||
+      checkDimension(812) ||
+      checkDimension(844) ||
+      checkDimension(896) ||
+      checkDimension(926))
   );
 }
 
 export function hasNotch() {
-  if (!device) {
-    return _hasNotchLegacy();
-  }
+  if (!device) return _hasNotchLegacy();
   return !!device.hasNotch;
 }
 
+function _hasDynamicIslandLegacy() {
+  return isIphone && (checkDimension(852) || checkDimension(932));
+}
+
 export function hasDynamicIsland() {
+  if (!device) return _hasDynamicIslandLegacy();
   return !!device?.hasDynamicIsland;
 }
 
@@ -83,16 +113,16 @@ export function getCutoutProps() {
   return device?.cutoutProps || EMPTY_OBJECT;
 }
 
-const checkDemension = (size) => {
+function checkDimension(size) {
   // window is not correct sometimes when screen is correct
   const window = Dimensions.get('window');
   const windowRes = window.width === size || window.height === size;
   const screen = Dimensions.get('screen');
   const screenRes = screen.width === size || screen.height === size;
   return windowRes || screenRes;
-};
+}
 
-const _getIphoneTopInset = (notchHeightOnly) => {
+function _getIphoneTopInset(notchHeightOnly) {
   if (hasNotch() || hasDynamicIsland()) {
     if (device) {
       return notchHeightOnly ? device.notchHeight : device.inset;
@@ -100,7 +130,7 @@ const _getIphoneTopInset = (notchHeightOnly) => {
     return topInset;
   }
   return 20;
-};
+}
 
 export function getTopInset(notchHeightOnly) {
   return Platform.select({
